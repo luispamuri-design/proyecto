ed9c80d399f0120ca6254b89d6a0186f7b0f4fb7
luispamuri-diseño <luispamuri@gmail.com>
2025-09-23 18:42:20 -0400

    Preparado para producción en Render

--- veterinaria/wsgi.py
+++ veterinaria/wsgi.py
@@ -1,8 +1,8 @@
 
 
 import os
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veterinaria.settings')
 
-application = get_asgi_application()
+application = get_wsgi_application()
